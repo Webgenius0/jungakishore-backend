741411b09eb1949c3c5411211b812b8249884897
helalscript <husoaib422@gmail.com>
2025-05-13 19:39:36 +0600

    feat: implement AreaFarmingType model with fillable attributes and casts

--- app/Models/AreaFarmingType.php
+++ app/Models/AreaFarmingType.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AreaFarmingType extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'area_farming_types';
+    protected $fillable = [
+        'title',
+        'slug',
+        'status',
+    ];
+
+    protected $casts = [
+        'title' => 'string',
+        'slug' => 'string',
+        'status' => 'string',
+    ];
 }
