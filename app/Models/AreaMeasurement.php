0b437b7b468d7479851b23f0d882af94539c1d5e
helalscript <husoaib422@gmail.com>
2025-05-13 19:39:40 +0600

    feat: define AreaMeasurement model with fillable attributes and casts

--- app/Models/AreaMeasurement.php
+++ app/Models/AreaMeasurement.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class AreaMeasurement extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'area_measurements';
+    protected $fillable = [
+        'title',
+        'slug',
+        'value',
+        'status',
+    ];
+
+    protected $casts = [
+        'title' => 'string',
+        'slug' => 'string',
+        'value' => 'decimal:2',
+        'status' => 'string',
+    ];
 }
