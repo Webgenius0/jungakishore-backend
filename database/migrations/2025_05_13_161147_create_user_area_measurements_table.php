03cd88e0d89a652b3f27130a4a670b79aaa42301
helalscript <husoaib422@gmail.com>
2025-05-13 19:40:08 +0600

    feat: add status column to area_measurements and user_area_measurements tables

--- database/migrations/2025_05_13_161147_create_user_area_measurements_table.php
+++ database/migrations/2025_05_13_161147_create_user_area_measurements_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('user_area_measurements', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('area_measurement_id')->constrained('area_measurements')->onDelete('cascade');
+            $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
         });
     }
